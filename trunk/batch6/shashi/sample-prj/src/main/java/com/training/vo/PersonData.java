7a8539975eba48d5d8da1d684d2b9666e4498114
shashitraining <shashitraining@3f5a6083-f9a2-4553-9860-34b9de827ca9>
2016-08-13 17:22:00 +0000

    changes to person data

--- trunk/batch6/shashi/sample-prj/src/main/java/com/training/vo/PersonData.java
+++ trunk/batch6/shashi/sample-prj/src/main/java/com/training/vo/PersonData.java
@@ -4,7 +4,14 @@ public class PersonData {
 	
 	private String firstName;
 	private String lastName;
-	private int Id;
+	private String middleName;
+	
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
