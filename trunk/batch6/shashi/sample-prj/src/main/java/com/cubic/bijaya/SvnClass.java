b49d7695211f2eb9653e65d8e683be7738e9aa73
Brookfe <Brookfe@3f5a6083-f9a2-4553-9860-34b9de827ca9>
2016-08-13 20:50:21 +0000

    Let's see if it works

--- trunk/batch6/shashi/sample-prj/src/main/java/com/cubic/bijaya/SvnClass.java
+++ trunk/batch6/shashi/sample-prj/src/main/java/com/cubic/bijaya/SvnClass.java
@@ -1,14 +1,15 @@
 package com.cubic.bijaya;
 
 public class SvnClass {
-	public static void main(String[] args) {
-		System.out.println("hello World");
-		System.out.println("Hello SVN");
-	}
-	public SvnClass(){
-		
-		
-		
+
+	public void print() {
+		System.out.println("It's working!!!");
 	}
 
+	public static void main(String[] args) {
+		System.out.println("hello World");
+
+		System.out.println("Hello SVN");
+
+	}
 }
