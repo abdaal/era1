e242df4d809907bf46188c53ab12c972b9913755
vs198907 <vs198907@3f5a6083-f9a2-4553-9860-34b9de827ca9>
2016-08-15 17:40:13 +0000

    added

--- src/main/java/com/vivek/Chceckin.java
+++ src/main/java/com/vivek/Chceckin.java
@@ -3,5 +3,24 @@ package com.vivek;
 public class Chceckin {
 	String firstName;
 	String lastName;
+	String address;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
